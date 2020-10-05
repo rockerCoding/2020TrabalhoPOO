9e95fbf1413962016d0e1ec301ae35f25b9eaebd
rockerCoding <44303811+rockerCoding@users.noreply.github.com>
2020-10-05 19:53:51 -0300

    Add files via upload

---
+++ ITime.java
@@ -0,0 +1,11 @@
+
+public interface ITime {
+	public void printNomeTime();
+	public void adicionarVitoria();
+	public void adicionarEmpate();
+	public void adicionarDerrota();
+	public int totalPontos();
+	public int getVitoria();
+	public int getEmpate();
+	public int getDerrota();
+}
